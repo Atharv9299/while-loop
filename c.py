afc53d74af604e1516bc4cb302292d36980ed638
Atharv9299 <atharv.rohal1113@gmail.com>
2025-10-17 19:19:39 +0530

    Add files via upload

---
+++ c.py
@@ -0,0 +1,26 @@
+# n = int (input("Enter a number:"))
+# sum = 0
+# i = 1
+# while i<=n :
+#     sum = sum+i
+#     i = i+1
+# print ("\n sum = ", sum)
+
+
+# i = 0
+# while i<=0
+#     print("ATHARV")
+
+
+num = int(input("Enter a number:"))
+sum = 0
+temp = num
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** 3
+    temp//= 10
+
+if num == sum:
+    print("It is an Armstrong number")
+else:
+    print("It is not an Armstrong number")
